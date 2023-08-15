2543a2288acd3e4b89c6b19bb3b327bbe0b92549
theshrish46 <theshrish46@gmail.com>
2023-08-15 23:25:20 +0530

    added auth

--- backend/index.js
+++ backend/index.js
@@ -2,26 +2,41 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const UserModel = require('./models/UserModel');
+
 require('dotenv').config();
 
-const conn = mongoose.connect(process.env.mongo_url);
-
 const app = express();
+
+mongoose.connect(process.env.mongodburl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.use(bodyParser.json());
 app.use(cors());
 
-app.post('/login', (req, res) => {
-  const { email, password } = req.body;
-  console.log(email, password);
-  
-  res.status(200).json({ name: email, password: password });
+const salt = bcrypt.genSaltSync(5);
+
+app.post('/register', async (req, res) => {
+  const { userName, email, password } = req.body;
+  try {
+    const userDoc = await UserModel.create({
+      name: userName,
+      email: email,
+      password: bcrypt.hashSync(password, salt),
+    });
+    res.status(201).json(userDoc);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
-app.post('/register', (req, res) => {
-  const { userName, email, password } = req.body;
-  console.log(userName, password, email);
-  res.status(201).json({ userName, password, email });
+app.post('/login', (req, res) => {
+  const { email, password } = req.body;
+  res.status(200).json({ name: email, password: password });
 });
 
 app.listen(process.env.PORT, () => {
--- backend/models/User.js
+++
@@ -1,9 +0,0 @@
-const { Schema, model } = require('mongoose');
-
-const UserSchema = Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true },
-  password: { type: String, required: true, select: false },
-});
-
-module.exports = User = model('User', UserSchema);
